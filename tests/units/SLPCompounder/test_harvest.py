8a20453cd23fe8b5d787d763d5705d771752e658
benber86 <benoitb@tutanota.de>
2021-07-16 16:31:46 +1000

    Tests for SLP harvest

--- tests/units/SLPCompounder/test_harvest.py
+++ tests/units/SLPCompounder/test_harvest.py
@@ -2,38 +2,36 @@ import brownie
 from brownie import chain
 import pytest
 
-DAY = 86400
 
+def test_harvest(sushi_lper1, sushi_lper2, slp_vault, slp, slp_compounder):
 
-@pytest.mark.parametrize("amount", [10**8, 10**12, 10**18])
-def test_harvest(alice, bob, vault, amount, alcx, alcx_pool, ss_compounder):
+    amount = 10**18
+    slp.approve(slp_vault, 2**256-1, {'from': sushi_lper1})
+    slp.approve(slp_vault, 2**256-1, {'from': sushi_lper2})
 
-    alcx.approve(vault, 2**256-1, {'from': alice})
-    alcx.approve(vault, 2**256-1, {'from': bob})
+    slp_vault.deposit(amount, {'from': sushi_lper1})
+    slp_vault.deposit(amount, {'from': sushi_lper2})
 
-    vault.deposit(amount, {'from': alice})
-    vault.deposit(amount, {'from': bob})
+    initial_alcx_balance_sushi_lper2 = slp.balanceOf(sushi_lper2)
+    initial_pv_sushi_lper1 = slp_vault.getPositionValue.call({'from': sushi_lper1})
+    initial_pv_sushi_lper2 = slp_vault.getPositionValue.call({'from': sushi_lper2})
 
-    initial_alcx_balance_bob = alcx.balanceOf(bob)
-    initial_pv_alice = vault.getPositionValue.call({'from': alice})
-    initial_pv_bob = vault.getPositionValue.call({'from': bob})
-
-    staked_prior = ss_compounder.stakeBalance()
-    total_prior = ss_compounder.totalPoolBalance()
+    staked_prior = slp_compounder.stakeBalance()
+    total_prior = slp_compounder.totalPoolBalance()
 
     chain.mine(1000)
 
-    assert ss_compounder.totalPoolBalance() > total_prior
-    assert ss_compounder.stakeBalance() == staked_prior
+    assert slp_compounder.totalPoolBalance() > total_prior
+    assert slp_compounder.stakeBalance() == staked_prior
 
-    ss_compounder.harvest({'from': bob})
+    slp_compounder.harvest({'from': sushi_lper2})
 
-    assert ss_compounder.totalPoolBalance() == ss_compounder.stakeBalance()
-    assert ss_compounder.stakeBalance() > staked_prior
+    assert slp_compounder.totalPoolBalance() == slp_compounder.stakeBalance()
+    assert slp_compounder.stakeBalance() > staked_prior
+    assert slp_compounder.getHarvestableSushi() == 0
 
-    assert alcx_pool.getStakeTotalUnclaimed(ss_compounder, 1) == 0
-    assert vault.getPositionValue.call({'from': bob}) > initial_pv_bob
-    assert vault.getPositionValue.call({'from': alice}) > initial_pv_alice
+    assert slp_vault.getPositionValue.call({'from': sushi_lper2}) > initial_pv_sushi_lper2
+    assert slp_vault.getPositionValue.call({'from': sushi_lper1}) > initial_pv_sushi_lper1
 
-    vault.withdrawAll({'from': bob})
-    assert alcx.balanceOf(bob) > initial_alcx_balance_bob
+    slp_vault.withdrawAll({'from': sushi_lper2})
+    assert slp.balanceOf(sushi_lper2) > initial_alcx_balance_sushi_lper2
